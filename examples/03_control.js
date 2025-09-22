9dee28f3d946e84c4712be734f4787148aea8f72
Alex Algarate <alex.algarate@gmail.com>
2025-09-22 23:02:54 +0200

    03 control (if, else, ternarias)

---
+++ examples/03_control.js
@@ -0,0 +1,51 @@
+console.log('03. Variables de control');
+
+const firstProductName = 'T-shirt';
+let firstProductPrice = 15;
+let firstProductQuantity = 2;
+
+const secondProductName = 'Pants';
+let secondProductPrice = 35;
+let secondProductQuantity = 2;
+
+let message = '';
+
+// Si el producto 1 > 2 "Primer producto más caro que el segundo"
+// Si es el mismo producto igual de precio
+// si es 1 < 2 primer producto es barato
+
+if (firstProductPrice > secondProductPrice) {
+  message = 'Primer producto más caro que el segundo';
+} else if (firstProductPrice === secondProductPrice) {
+  message = 'mismo precio';
+} else {
+  message = 'primer producto es barato'; // se ejecuta esta condición
+}
+console.log(message);
+secondProductPrice = 15;
+if (firstProductPrice > secondProductPrice) {
+  message = 'Primer producto más caro que el segundo';
+} else if (firstProductPrice === secondProductPrice) {
+  message = 'mismo precio'; // se ejecuta esta condición
+} else {
+  message = 'primer producto es barato';
+}
+console.log(message);
+
+firstProductPrice = 20;
+if (firstProductPrice > secondProductPrice) {
+  message = 'Primer producto más caro que el segundo'; // se ejecuta esta condición
+} else if (firstProductPrice === secondProductPrice) {
+  message = 'mismo precio';
+} else {
+  message = 'primer producto es barato';
+}
+
+console.log(message);
+
+// operador ternario
+const sameName = firstProductName === secondProductName;
+const samePrice = firstProductPrice === secondProductPrice;
+
+const message2 = sameName && samePrice ? 'Mismos productos' : 'Son diferentes';
+console.log(message2);
