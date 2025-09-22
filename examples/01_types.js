09983a8740dbd3843945f885172f6b7d25b78073
Alex Algarate <alex.algarate@gmail.com>
2025-09-22 21:04:56 +0200

    types of variables

---
+++ examples/01_types.js
@@ -0,0 +1,15 @@
+const precio = 192.98;
+
+const producto = 'Zapatillas';
+const precioProducto = 199.99;
+
+const producto2 = 'Camisa';
+const precioProducto2 = 29.99;
+const descuento = false;
+
+console.log(typeof precio); // number
+console.log(typeof producto); // string
+console.log(typeof descuento); // boolean
+
+let disponible;
+console.log(typeof disponible); // undefined
--- examples/helloWorld.js
+++ examples/helloWorld.js
@@ -1,1 +1,16 @@
 console.log('Hello world desde el fichero helloWorld.js en la carpeta examples');
+
+// Variables
+let num1 = 1;
+const num2 = 2;
+
+console.log(num1, num2);
+
+num1 = 10;
+console.log(`El nuevo valor de num1 es ${num1}`);
+
+// num2 = 1;
+// console.log(`El nuevo valor de num1 es ${num2}`);
+// Este código no lo va a ejecutar porque hay un error en la
+// línea 12 al intentar asignar un nuevo valor a una constante
+// console.log(`El nuevo valor de num1 es ${num1}`);
