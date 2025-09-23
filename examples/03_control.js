05a52c8416481d83eb486bf9490c52539eb37fd4
Alex Algarate <alex.algarate@gmail.com>
2025-09-23 19:54:46 +0200

    03_Control: Falsy-truthy

--- examples/03_control.js
+++ examples/03_control.js
@@ -138,3 +138,24 @@ if (total < 50) {
 }
 
 console.log(`Total: ${total} || Message: ${messageExercise}`);
+
+// Falsy-truthy
+
+const valor1 = 32;
+const valor2 = 'hola';
+
+let isTruthy = valor1 ? 'Es verdadero' : 'Es falso';
+
+console.log(isTruthy);
+
+const valor3 = 0;
+const valor4 = '';
+const valor5 = null;
+const valor6 = undefined;
+const valor7 = NaN;
+
+console.log(valor3, ' -- ', valor3 ? 'Es verdadero' : 'Es falso');
+console.log(valor4, ' -- ', valor4 ? 'Es verdadero' : 'Es falso');
+console.log(valor5, ' -- ', valor5 ? 'Es verdadero' : 'Es falso');
+console.log(valor6, ' -- ', valor6 ? 'Es verdadero' : 'Es falso');
+console.log(valor7, ' -- ', valor7 ? 'Es verdadero' : 'Es falso');
