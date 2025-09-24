ce7247c1196202290e80d4ec539be2c87dc015f8
Alex Algarate <alex.algarate@gmail.com>
2025-09-24 19:42:47 +0200

    Resolving exercise 3.2

--- practica/3_ejercicio.js
+++ practica/3_ejercicio.js
@@ -22,3 +22,19 @@ const input3 = [
 ];
 createPath(input3);
 // 'programming/languages/easy/beginner/useful/pythonstuff.py'
+
+/*3.2 Ejercicio
+Crea una función para que con estos datos de entrada se produzca los siguientes
+resultados:
+*/
+
+const input = 10;
+// create your function here
+const dashSeparator = (input) => {
+  console.log(input.toString().split('').join('-'));
+};
+dashSeparator(input); // '1-0'
+const secondInput = 1;
+dashSeparator(secondInput); // '1'
+const thirdInput = 11234;
+dashSeparator(thirdInput); // '1-1-2-3-4'
