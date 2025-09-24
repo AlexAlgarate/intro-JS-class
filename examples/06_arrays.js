2e5b3576529c202d12aac3360b5dd721797d9a08
Alex Algarate <alex.algarate@gmail.com>
2025-09-24 18:35:05 +0200

    06_arrays: intro arrays

---
+++ examples/06_arrays.js
@@ -0,0 +1,10 @@
+const myProducts = ['TV', 'PC', 'Cap', 'T-shirt'];
+
+console.log('Primer producto: ', myProducts[0]); // TV
+console.log('Segundo producto: ', myProducts[1]); // PC
+console.log('Tercer producto: ', myProducts[2]); // Cap
+console.log('Cuarto producto: ', myProducts[3]); // T-Shirt
+console.log('Producto que no existe en el array: ', myProducts[4]); // undefined
+
+const firstProduct = myProducts[0];
+console.log(firstProduct.toLowerCase()); // tv
